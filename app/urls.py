92d887eb1b8558f75d818a0e68e9331b510de21a
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-12-06 16:26:54 -0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,1 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from clientes.views import ClienteViewSet
+from transacoes.views import TransacaoViewSet
 
+router = DefaultRouter()
+router.register(r'clientes', ClienteViewSet)
+router.register(r'transacoes', TransacaoViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+]
