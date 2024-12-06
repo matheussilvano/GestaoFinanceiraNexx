7b475ba666bfd3fb22632910a61f9deee29771f6
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-12-06 16:19:15 -0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -1,1 +1,11 @@
-
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'rest_framework',
+    'drf_yasg',
+    'clientes',
+    'transacoes',
+    'relatorios',
+]
