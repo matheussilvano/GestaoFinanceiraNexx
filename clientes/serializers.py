584e52c120f55b2f3a2932503116701893787b19
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-12-06 16:25:15 -0300

    Update serializers.py

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,1 +1,8 @@
+from rest_framework import serializers
+from .models import Cliente
 
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = ['id', 'cpf', 'nome', 'email', 'data_criacao']
+        read_only_fields = ['id', 'data_criacao']
